f04f4492a99c11fd7186c5ae993381a18680902d
imranshafin <imranshafin004@gmail.com>
2025-10-30 15:05:39 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 377 ;
+        int num2 = 6754;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
+
