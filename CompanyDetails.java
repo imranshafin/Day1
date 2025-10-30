bf8bbf2acaa104ae333f840bd1c63d465a3479a3
imranshafin <imranshafin004@gmail.com>
2025-10-30 14:56:54 +0530

    Add files via upload

---
+++ CompanyDetails.java
@@ -0,0 +1,8 @@
+public class CompanyDetails {
+    public static void main(String[] args) {
+        String company = "How";
+        String city = "You?";
+        System.out.println(company + " are " + city);
+    }
+}
+
